4a55dbfd218a1d6a3619d49faee2aef26049d4dd
WilliamG <williamalberto1@hotmail.com>
2023-02-05 11:26:21 -0500

    Incluyendo animacion con el scroll se definieron ID

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 window.addEventListener('scroll', function(){
-    console.log('Aqui Estoy....');
-    let animateMenu1 = document.getElementById('prueba');
-    console.log(animateMenu1 === 'object');
-    console.log(animateMenu1 !== null);
-    console.log('getBoundingClientRect' in animateMenu1)
-    let pos = animateMenu1.getBoundingClientRect().top;
-    console.log(pos);
+    
+    let animateMenu1 = document.getElementById('menu1');
+    let posMenu1 = animateMenu1.getBoundingClientRect().top;
+    console.log(posMenu1);
+    console.log(posMenu1 < 5);
+    if (posMenu1 < 5){
+        console.log('Aqui Estoy....');
+        animateMenu1.style.left = '50em';
+    }
 
 })
