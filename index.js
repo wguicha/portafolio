a6aa98087b3e280dfc513a659598e766937e2763
WilliamG <williamalberto1@hotmail.com>
2023-02-05 09:23:20 -0500

    Incluyendo animacion con el scrll

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-window.onload = function () {
-    setTimeout(() => {
-        document.querySelector(".menu").style.visibility = "visible";
-    }, 6000);
-}
+window.addEventListener('scroll', function(){
+    console.log('Aqui Estoy....');
+    let animateMenu1 = document.getElementById('prueba');
+    console.log(animateMenu1 === 'object');
+    console.log(animateMenu1 !== null);
+    console.log('getBoundingClientRect' in animateMenu1)
+    let pos = animateMenu1.getBoundingClientRect().top;
+    console.log(pos);
+
+})
