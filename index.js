f041246bba06678b1b4a90bea3a9b0d9cc1cb82e
WilliamG <williamalberto1@hotmail.com>
2023-02-07 20:55:26 -0500

    Se creo la animacion para el cambio entre barras por movimiento del scroll

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 window.addEventListener('scroll', function(){
     
-    let animateMenu1 = document.getElementById('menu1');
-    let posMenu1 = animateMenu1.getBoundingClientRect().top;
-    console.log(posMenu1);
-    console.log(posMenu1 < 5);
-    if (posMenu1 < 5){
+    let container = document.getElementById('container');
+    let head = document.getElementById('head');
+    let bar2 = document.getElementById('bar2');
+    let posContainer = container.getBoundingClientRect().top;
+    console.log(posContainer);
+    if (posContainer < -80){
         console.log('Aqui Estoy....');
+        head.style.display = 'none';
+        bar2.style.display = 'inline-block'
+    }else{
+        head.style.display = 'inline-block';
+        bar2.style.display = 'none'
     }
 
 })
